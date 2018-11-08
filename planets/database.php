b34212bc2befbc795a8adc60ef8b7749f8857352
lantos14 <lantos891213@gmail.com>
2018-11-08 11:29:03 +0100

    feat: add some php foolaround

---
+++ planets/database.php
@@ -0,0 +1,33 @@
+<?php
+$planets = array(
+  "Mercury" => array(
+    "name" => "Mercury",
+    "img-src" => "https://bit.ly/2DqC8Sf",
+    "radius" => 2400,
+    "gravity" => 3.7,
+    "atmosphere" => array("No atmosphere")
+  ),
+  "Venus" => array(
+    "name" => "Venus",
+    "img-src" => "https://bit.ly/2QqES5d",
+    "radius" => 6000,
+    "gravity" => 8.87,
+    "atmosphere" => array("CO2", "N")
+  ),
+  "Earth" => array(
+    "name" => "Earth",
+    "img-src" => "https://bit.ly/2rbB3Gd",
+    "radius" => 6400,
+    "gravity" => 9.8,
+    "atmosphere" => array("O", "N")
+  ),
+  "Mars" => array(
+    "name" => "Mars",
+    "img-src" => "https://bit.ly/2SUy5lD",
+    "radius" => 3400,
+    "gravity" => 3.7,
+    "atmosphere" => array("H", "He")
+  ),
+);
+
+?>
--- planets/result.php
+++ planets/result.php
@@ -12,47 +12,38 @@
   <div id="main">
 
     <?php
+      include 'database.php'; // import planet data
 
       $selectedPlanet = $_POST["Planet"];
-      $planets = array(
-        "Mercury" => array(
-          "name" => "Mercury",
-          "img-src" => "https://bit.ly/2DqC8Sf",
-          "radius" => 2400,
-          "gravity" => 3.7,
-          "atmosphere" => null
-        ),
-        "Venus" => array(
-          "name" => "Venus",
-          "img-src" => "https://bit.ly/2QqES5d",
-          "radius" => 6000,
-          "gravity" => 8.87,
-          "atmosphere" => array("CO2", "N")
-        ),
-        "Earth" => array(
-          "name" => "Earth",
-          "img-src" => "https://bit.ly/2rbB3Gd",
-          "radius" => 6400,
-          "gravity" => 9.8,
-          "atmosphere" => array("O", "N")
-        ),
-        "Mars" => array(
-          "name" => "Mars",
-          "img-src" => "https://bit.ly/2SUy5lD",
-          "radius" => 3400,
-          "gravity" => 3.7,
-          "atmosphere" => array("H", "He")
-        ),
-      );
 
+      function getAtmosphere($atmo) {
+        foreach ($atmo as $key) {
+          return $key;
+        }
+      }
     ?>
 
     <div>
       <h2>Planet Information</h2>
       <p>Name: <strong> <?php echo $selectedPlanet?> </strong></p>
-      <p>Equatorial Radius: <strong> <?php echo $planets[$selectedPlanet]["radius"]." km"?> </strong></p>
-      <p>Gravity: <strong> <?php echo $planets[$selectedPlanet]["gravity"]." m/s2"?></strong></p>
-      <img src=<?php echo $planets[$selectedPlanet]["img-src"]?> alt="planet">
+      <p>Equatorial Radius: 
+        <strong> 
+          <?php echo $planets[$selectedPlanet]["radius"]." km"?> 
+        </strong>
+      </p>
+      <p>Gravity: 
+        <strong> 
+        <?php echo $planets[$selectedPlanet]["gravity"]." m/s2"?>
+      </strong>
+    </p>
+      <p>Atmosphere composition: 
+        <strong> 
+          <?php echo getAtmosphere($planets[$selectedPlanet]["atmosphere"])?>
+        </strong>
+      </p>
+      <img 
+      src=<?php echo $planets[$selectedPlanet]["img-src"]?> 
+      alt="planet">
     </div>
   
   </div>
